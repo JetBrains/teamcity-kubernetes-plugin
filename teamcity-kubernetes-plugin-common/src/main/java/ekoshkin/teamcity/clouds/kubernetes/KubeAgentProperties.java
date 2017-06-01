c9686194404a688f27d0a371bd7435be43cecb16
ekoshkin <koshkinev@gmail.com>
2017-06-01 14:51:47 +0300

    rename kube agent props

--- teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeAgentProperties.java
+++ teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeAgentProperties.java
@@ -4,6 +4,6 @@ package ekoshkin.teamcity.clouds.kubernetes;
  * Created by ekoshkin (koshkinev@gmail.com) on 30.05.17.
  */
 public class KubeAgentProperties {
-    public static final String IMAGE_NAME = "image-name";
-    public static final String INSTANCE_NAME = "instance-name";
+    public static final String IMAGE_NAME = "kube-image-name";
+    public static final String INSTANCE_NAME = "kube-instance-name";
 }
