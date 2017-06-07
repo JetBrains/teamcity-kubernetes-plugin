ffe1376b4eb166292404d0fe8c8e01d6bc72e13b
ekoshkin <koshkinev@gmail.com>
2017-06-07 12:35:47 +0300

    update agent configuration with kubernetes specific parameter passed to container when starting pod

--- teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeAgentProperties.java
+++ teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeAgentProperties.java
@@ -4,6 +4,6 @@ package ekoshkin.teamcity.clouds.kubernetes;
  * Created by ekoshkin (koshkinev@gmail.com) on 30.05.17.
  */
 public class KubeAgentProperties {
-    public static final String IMAGE_NAME = "kube-image-name";
-    public static final String INSTANCE_NAME = "kube-instance-name";
+    public static final String IMAGE_NAME = "teamcity.kubernetes.imageName";
+    public static final String INSTANCE_NAME = "teamcity.kubernetes.instanceName";
 }
