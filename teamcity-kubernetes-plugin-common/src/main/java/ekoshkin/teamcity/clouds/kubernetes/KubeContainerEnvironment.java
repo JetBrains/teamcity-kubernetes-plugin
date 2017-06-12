3ef0b8573cf074944a41e97c3ae1340d5c399dda
ekoshkin <koshkinev@gmail.com>
2017-06-13 00:50:09 +0300

    teamcity kubernetes env to upper case

--- teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeContainerEnvironment.java
+++ teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeContainerEnvironment.java
@@ -4,8 +4,10 @@ package ekoshkin.teamcity.clouds.kubernetes;
  * Created by ekoshkin (koshkinev@gmail.com) on 07.06.17.
  */
 public class KubeContainerEnvironment {
-    public static final String AGENT_NAME = "teamcity_kubernetes_agentName";
-    public static final String SERVER_URL = "teamcity_kubernetes_serverUrl";
-    public static final String IMAGE_NAME = "teamcity_kubernetes_imageName";
-    public static final String INSTANCE_NAME = "teamcity_kubernetes_instanceName";
+    public static final String AGENT_NAME = "TEAMCITY_KUBERNETES_AGENT_NAME";
+    public static final String SERVER_URL = "TEAMCITY_KUBERNETES_SERVER_URL";
+    public static final String IMAGE_NAME = "TEAMCITY_KUBERNETES_IMAGE_NAME";
+    public static final String INSTANCE_NAME = "TEAMCITY_KUBERNETES_INSTANCE_NAME";
+
+    public static final String OFFICIAL_IMAGE_SERVER_URL = "SERVER_URL";
 }
