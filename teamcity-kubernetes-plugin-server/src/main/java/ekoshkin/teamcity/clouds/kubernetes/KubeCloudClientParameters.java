1a1b71056a4c2b4ee0d0a3f32457942dfbc56461
ekoshkin <koshkinev@gmail.com>
2017-06-12 22:51:41 +0300

    explicictly pass 'default' namespace if not specified

--- teamcity-kubernetes-plugin-server/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeCloudClientParameters.java
+++ teamcity-kubernetes-plugin-server/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeCloudClientParameters.java
@@ -5,6 +5,7 @@ import jetbrains.buildServer.clouds.CloudClientParameters;
 import jetbrains.buildServer.clouds.CloudImageParameters;
 import jetbrains.buildServer.util.CollectionsUtil;
 import jetbrains.buildServer.util.Converter;
+import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -15,6 +16,8 @@ import static ekoshkin.teamcity.clouds.kubernetes.KubeParametersConstants.*;
  * Created by ekoshkin (koshkinev@gmail.com) on 29.05.17.
  */
 public class KubeCloudClientParameters implements KubeApiConnection {
+    private static final String DEFAULT_NAMESPACE = "default";
+
     private final CloudClientParameters myParameters;
 
     public KubeCloudClientParameters(CloudClientParameters parameters) {
@@ -45,7 +48,8 @@ public class KubeCloudClientParameters implements KubeApiConnection {
     }
 
     public String getNamespace(){
-        return myParameters.getParameter(KUBERNETES_NAMESPACE);
+        String explicitNameSpace = myParameters.getParameter(KUBERNETES_NAMESPACE);
+        return StringUtil.isEmpty(explicitNameSpace) ? DEFAULT_NAMESPACE : explicitNameSpace;
     }
 
     public Collection<KubeCloudImageData> getImages(){
