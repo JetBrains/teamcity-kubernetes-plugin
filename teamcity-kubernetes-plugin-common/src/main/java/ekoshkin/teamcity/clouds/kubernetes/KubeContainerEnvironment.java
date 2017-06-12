7c03727b5a9c955373e9c63466033e63b8168f19
ekoshkin <koshkinev@gmail.com>
2017-06-12 22:51:05 +0300

    fix pod spec validation error

--- teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeContainerEnvironment.java
+++ teamcity-kubernetes-plugin-common/src/main/java/ekoshkin/teamcity/clouds/kubernetes/KubeContainerEnvironment.java
@@ -4,8 +4,8 @@ package ekoshkin.teamcity.clouds.kubernetes;
  * Created by ekoshkin (koshkinev@gmail.com) on 07.06.17.
  */
 public class KubeContainerEnvironment {
-    public static final String AGENT_NAME = "teamcity.kubernetes.agentName";
-    public static final String SERVER_URL = "teamcity.kubernetes.serverUrl";
-    public static final String IMAGE_NAME = "teamcity.kubernetes.imageName";
-    public static final String INSTANCE_NAME = "teamcity.kubernetes.instanceName";
+    public static final String AGENT_NAME = "teamcity_kubernetes_agentName";
+    public static final String SERVER_URL = "teamcity_kubernetes_serverUrl";
+    public static final String IMAGE_NAME = "teamcity_kubernetes_imageName";
+    public static final String INSTANCE_NAME = "teamcity_kubernetes_instanceName";
 }
