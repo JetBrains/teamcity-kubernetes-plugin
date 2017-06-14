90e97d82b86ce57dfc9a1a614648d2670827638c
ekoshkin <koshkinev@gmail.com>
2017-06-15 00:18:45 +0300

    user password auth strategy applied

--- teamcity-kubernetes-plugin-server/src/main/java/ekoshkin/teamcity/clouds/kubernetes/auth/UserPasswdAuthStrategy.java
+++ teamcity-kubernetes-plugin-server/src/main/java/ekoshkin/teamcity/clouds/kubernetes/auth/UserPasswdAuthStrategy.java
@@ -1,5 +1,6 @@
 package ekoshkin.teamcity.clouds.kubernetes.auth;
 
+import ekoshkin.teamcity.clouds.kubernetes.connector.KubeApiConnection;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -8,16 +9,18 @@ import org.jetbrains.annotations.Nullable;
  * Created by ekoshkin (koshkinev@gmail.com) on 14.06.17.
  */
 public class UserPasswdAuthStrategy implements KubeAuthStrategy {
+    public static final String ID = "user-passwd";
+
     @NotNull
     @Override
     public String getId() {
-        return null;
+        return ID;
     }
 
     @NotNull
     @Override
     public String getDisplayName() {
-        return null;
+        return "Username / Password";
     }
 
     @Nullable
@@ -26,8 +29,9 @@ public class UserPasswdAuthStrategy implements KubeAuthStrategy {
         return null;
     }
 
+    @NotNull
     @Override
-    public void apply(@NotNull ConfigBuilder clientConfig) {
-
+    public ConfigBuilder apply(@NotNull ConfigBuilder clientConfig, @NotNull KubeApiConnection connection) {
+        return clientConfig.withUsername(connection.getUsername()).withPassword(connection.getPassword());
     }
 }
