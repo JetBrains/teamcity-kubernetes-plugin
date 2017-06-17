0c81a837a910032cf6fb449f40e9a128d82f646a
ekoshkin <koshkinev@gmail.com>
2017-06-17 12:33:07 +0300

    first test added

---
+++ teamcity-kubernetes-plugin-server/src/main/test/ekoshkin/teamcity/clouds/kubernetes/podSpec/CustomTemplatePodTemplateProviderTest.java
@@ -0,0 +1,24 @@
+package ekoshkin.teamcity.clouds.kubernetes.podSpec;
+
+import jetbrains.buildServer.BaseTestCase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ekoshkin (koshkinev@gmail.com) on 17.06.17.
+ */
+public class CustomTemplatePodTemplateProviderTest extends BaseTestCase {
+    private PodTemplateProvider myPodTemplateProvider;
+
+    @BeforeMethod
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        myPodTemplateProvider = new CustomTemplatePodTemplateProvider();
+    }
+
+    @Test
+    public void testGetPodTemplate() throws Exception {
+        assertNotNull(myPodTemplateProvider.getPodTemplate(null, null, null));
+    }
+}
