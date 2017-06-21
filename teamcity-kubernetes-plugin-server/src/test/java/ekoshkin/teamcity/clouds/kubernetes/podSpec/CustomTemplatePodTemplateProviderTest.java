50139cade778955ed0b159385b589f8a8f427c0b
ekoshkin <koshkinev@gmail.com>
2017-06-21 15:33:09 +0300

    couple of tests

--- teamcity-kubernetes-plugin-server/src/test/java/ekoshkin/teamcity/clouds/kubernetes/podSpec/CustomTemplatePodTemplateProviderTest.java
+++ teamcity-kubernetes-plugin-server/src/test/java/ekoshkin/teamcity/clouds/kubernetes/podSpec/CustomTemplatePodTemplateProviderTest.java
@@ -1,24 +1,84 @@
 package ekoshkin.teamcity.clouds.kubernetes.podSpec;
 
+import ekoshkin.teamcity.clouds.kubernetes.KubeCloudClientParameters;
+import ekoshkin.teamcity.clouds.kubernetes.KubeCloudImage;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import jetbrains.buildServer.BaseTestCase;
+import jetbrains.buildServer.clouds.CloudInstanceUserData;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Collections;
 
 /**
  * Created by ekoshkin (koshkinev@gmail.com) on 17.06.17.
  */
 public class CustomTemplatePodTemplateProviderTest extends BaseTestCase {
     private PodTemplateProvider myPodTemplateProvider;
+    private Mockery m;
 
     @BeforeMethod
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        m = new Mockery();
         myPodTemplateProvider = new CustomTemplatePodTemplateProvider();
     }
 
-    @Test(enabled = false)
+    @AfterMethod
+    @Override
+    protected void tearDown() throws Exception {
+        m.assertIsSatisfied();
+        super.tearDown();
+    }
+
+    @Test
+    public void testGetPodTemplate_invalidJSON() throws Exception {
+        CloudInstanceUserData instanceTag = createInstanceTag();
+        KubeCloudClientParameters clientParams = m.mock(KubeCloudClientParameters.class);
+        KubeCloudImage image = m.mock(KubeCloudImage.class);
+        m.checking(new Expectations(){{
+            allowing(image).getCustomPodTemplateSpec(); will(returnValue("foo"));
+        }});
+        assertExceptionThrown(() -> myPodTemplateProvider.getPodTemplate(instanceTag, image, clientParams), KubernetesClientException.class);
+    }
+
+    @Test
     public void testGetPodTemplate() throws Exception {
-        assertNotNull(myPodTemplateProvider.getPodTemplate(null, null, null));
+        CloudInstanceUserData instanceTag = createInstanceTag();
+        KubeCloudClientParameters clientParams = m.mock(KubeCloudClientParameters.class);
+        KubeCloudImage image = m.mock(KubeCloudImage.class);
+        m.checking(new Expectations(){{
+            allowing(image).getCustomPodTemplateSpec(); will(returnValue("{\n" +
+                    "\t\"template\": {\n" +
+                    "\t\t\"metadata\": {\n" +
+                    "\t\t\t\"labels\": {\n" +
+                    "\t\t\t\t\"app\": \"nginx\"\n" +
+                    "\t\t\t}\n" +
+                    "\t\t},\n" +
+                    "\t\t\"spec\": {\n" +
+                    "\t\t\t\"containers\": [\n" +
+                    "\t\t\t\t{\n" +
+                    "\t\t\t\t\t\"name\": \"nginx\",\n" +
+                    "\t\t\t\t\t\"image\": \"nginx:1.7.9\",\n" +
+                    "\t\t\t\t\t\"ports\": [\n" +
+                    "\t\t\t\t\t\t{\n" +
+                    "\t\t\t\t\t\t\t\"containerPort\": 80\n" +
+                    "\t\t\t\t\t\t}\n" +
+                    "\t\t\t\t\t]\n" +
+                    "\t\t\t\t}\n" +
+                    "\t\t\t]\n" +
+                    "\t\t}\n" +
+                    "\t}\n" +
+                    "}"));
+        }});
+        assertNotNull(myPodTemplateProvider.getPodTemplate(instanceTag, image, clientParams));
+    }
+
+    private CloudInstanceUserData createInstanceTag() {
+        return new CloudInstanceUserData("foo", "foo", "foo", null, "foo", "foo", Collections.emptyMap());
     }
 }
