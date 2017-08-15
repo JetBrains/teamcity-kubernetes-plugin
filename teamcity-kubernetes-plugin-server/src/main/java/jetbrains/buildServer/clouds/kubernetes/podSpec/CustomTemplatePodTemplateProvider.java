59403e0f5a41502ca12ae6f341097cedfca79db9
ekoshkin <koshkinev@gmail.com>
2017-08-15 17:30:00 +0300

    do not set container name when building pod based on custom template

--- teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/podSpec/CustomTemplatePodTemplateProvider.java
+++ teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/podSpec/CustomTemplatePodTemplateProvider.java
@@ -70,8 +70,6 @@ public class CustomTemplatePodTemplateProvider implements PodTemplateProvider {
 
         PodSpec spec = podTemplateSpec.getSpec();
         for (Container container : spec.getContainers()){
-            container.setName(agentName);
-
             Map<String, String> patchedEnvData = new HashMap<>();
             for (EnvVar env : container.getEnv()){
                 patchedEnvData.put(env.getName(), env.getValue());
--- teamcity-kubernetes-plugin-server/src/test/java/jetbrains/buildServer/clouds/kubernetes/podSpec/CustomTemplatePodTemplateProviderTest.java
+++ teamcity-kubernetes-plugin-server/src/test/java/jetbrains/buildServer/clouds/kubernetes/podSpec/CustomTemplatePodTemplateProviderTest.java
@@ -1,5 +1,6 @@
 package jetbrains.buildServer.clouds.kubernetes.podSpec;
 
+import io.fabric8.kubernetes.api.model.Container;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import jetbrains.buildServer.BaseTestCase;
@@ -86,6 +87,42 @@ public class CustomTemplatePodTemplateProviderTest extends BaseTestCase {
         assertNotNull(podTemplate);
         assertNotNull(podTemplate.getMetadata());
         assertNotNull(podTemplate.getSpec());
+    }
+
+    @Test
+    public void testShouldNotsetContainerName(){
+        CloudInstanceUserData instanceTag = createInstanceTag();
+        KubeCloudClientParameters clientParams = m.mock(KubeCloudClientParameters.class);
+        KubeCloudImage image = m.mock(KubeCloudImage.class);
+        m.checking(new Expectations(){{
+            allowing(clientParams).getNamespace(); will(returnValue("custom namespace"));
+            allowing(image).getId(); will(returnValue("my image id"));
+            allowing(image).getName(); will(returnValue("my image name"));
+            allowing(image).getCustomPodTemplateSpec(); will(returnValue("{\n" +
+                    "\t\t\"metadata\": {\n" +
+                    "\t\t\t\"labels\": {\n" +
+                    "\t\t\t\t\"app\": \"nginx\"\n" +
+                    "\t\t\t}\n" +
+                    "\t\t},\n" +
+                    "\t\t\"spec\": {\n" +
+                    "\t\t\t\"containers\": [\n" +
+                    "\t\t\t\t{\n" +
+                    "\t\t\t\t\t\"name\": \"nginx\",\n" +
+                    "\t\t\t\t\t\"image\": \"nginx:1.7.9\",\n" +
+                    "\t\t\t\t\t\"ports\": [\n" +
+                    "\t\t\t\t\t\t{\n" +
+                    "\t\t\t\t\t\t\t\"containerPort\": 80\n" +
+                    "\t\t\t\t\t\t}\n" +
+                    "\t\t\t\t\t]\n" +
+                    "\t\t\t\t}\n" +
+                    "\t\t\t]\n" +
+                    "\t\t}\n" +
+                    "}"));
+        }});
+        Pod podTemplate = myPodTemplateProvider.getPodTemplate(instanceTag, image, clientParams);
+        for(Container container : podTemplate.getSpec().getContainers()){
+            assertNotSame(container.getName(), "agent name");
+        }
     }
 
     private CloudInstanceUserData createInstanceTag() {
