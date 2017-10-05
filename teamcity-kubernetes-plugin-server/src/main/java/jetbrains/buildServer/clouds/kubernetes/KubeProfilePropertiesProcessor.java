fc66a889abd8de2279cb5bb25bc8b0d15c96b4a8
ekoshkin <koshkinev@gmail.com>
2017-10-05 17:26:58 +0300

    fix TW-51790, validate profile

--- teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/KubeProfilePropertiesProcessor.java
+++ teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/KubeProfilePropertiesProcessor.java
@@ -2,10 +2,14 @@ package jetbrains.buildServer.clouds.kubernetes;
 
 import jetbrains.buildServer.serverSide.InvalidProperty;
 import jetbrains.buildServer.serverSide.PropertiesProcessor;
+import jetbrains.buildServer.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
+
+import static jetbrains.buildServer.clouds.kubernetes.KubeParametersConstants.API_SERVER_URL;
+import static jetbrains.buildServer.clouds.kubernetes.KubeParametersConstants.AUTH_STRATEGY;
 
 /**
  * Created by ekoshkin (koshkinev@gmail.com) on 27.05.17.
@@ -13,6 +17,9 @@ import java.util.Map;
 public class KubeProfilePropertiesProcessor implements PropertiesProcessor {
     @Override
     public Collection<InvalidProperty> process(Map<String, String> map) {
-        return Collections.emptyList();
+        Collection<InvalidProperty> invalids = new ArrayList<>();
+        if(StringUtil.isEmptyOrSpaces(map.get(API_SERVER_URL))) invalids.add(new InvalidProperty(API_SERVER_URL, "Kubernetes API server URL must not be empty"));
+        if(StringUtil.isEmptyOrSpaces(map.get(AUTH_STRATEGY))) invalids.add(new InvalidProperty(AUTH_STRATEGY, "Authentication strategy must be selected"));
+        return invalids;
     }
 }
