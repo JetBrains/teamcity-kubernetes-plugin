6f691ac33de211d6cf5110caca817165b6e2eb14
Pavel Sher <pavel.sher@jetbrains.com>
2025-12-04 13:39:41 +0000

    TW-88627: avoid producing env.TC_K8S_PROVIDED_teamcity_agent_startingInstanceId configuration parameter and remove env.TC_K8S_STARTING_INSTANCE_ID as soon as the build is started
    + switch to a more performant method in the place where we look up for an agent parameter
    
    
    Merge-request: TC-MR-19393
    Merged-by: Pavel Sher <Pavel.Sher@jetbrains.com>

--- teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/podSpec/BuildAgentPodTemplateProvider.java
+++ teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/podSpec/BuildAgentPodTemplateProvider.java
@@ -6,6 +6,7 @@ import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
 import io.fabric8.kubernetes.api.model.Pod;
 import java.util.List;
 import java.util.stream.Collectors;
+import jetbrains.buildServer.agent.AgentRuntimeProperties;
 import jetbrains.buildServer.clouds.CloudInstanceUserData;
 import jetbrains.buildServer.clouds.kubernetes.KubeCloudImage;
 import jetbrains.buildServer.clouds.kubernetes.KubeContainerEnvironment;
@@ -29,8 +30,9 @@ public interface BuildAgentPodTemplateProvider {
 
     static @NotNull List<EnvVar> generateCustomAgentEnvVars(@NotNull CloudInstanceUserData cloudInstanceUserData) {
       return cloudInstanceUserData.getCustomAgentConfigurationParameters().entrySet().stream()
-                                  .filter(entry -> !(entry.getKey().startsWith(KubeContainerEnvironment.TEAMCITY_KUBERNETES_PREFIX) ||
-                                                     entry.getKey().startsWith(KubeContainerEnvironment.TEAMCITY_KUBERNETES_PROVIDED_PREFIX)))
+                                  .filter(entry -> !entry.getKey().startsWith(KubeContainerEnvironment.TEAMCITY_KUBERNETES_PREFIX))
+                                  .filter(entry -> !entry.getKey().startsWith(KubeContainerEnvironment.TEAMCITY_KUBERNETES_PROVIDED_PREFIX))
+                                  .filter(entry -> !entry.getKey().equals(AgentRuntimeProperties.STARTING_CLOUD_INSTANCE_ID))
                                   .map(entry ->
                                          new EnvVar(KubeContainerEnvironment.TEAMCITY_KUBERNETES_PROVIDED_PREFIX + KubeContainerEnvironment.paramToEnvVar(entry.getKey()),
                                                     entry.getValue(), null)
