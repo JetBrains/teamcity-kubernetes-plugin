3042dc2a091461713c71b48624010ba67cadae07
Kirill (KIR) Maximov <kir@maxkir.com>
2026-07-27 14:24:43 +0200

    TW-102446 SSRF in Kubernetes ChooserController (`/plugins/teamcity-kubernetes-plugin/kubeNamespaces.html`) allows any authenticated TeamCity user to make arbitrary server-side requests with a response oracle
    (cherry picked from commit 2ea545b8bd3cfc84e716a0b0594ff581f6fe55d2)

--- teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/web/ChooserController.java
+++ teamcity-kubernetes-plugin-server/src/main/java/jetbrains/buildServer/clouds/kubernetes/web/ChooserController.java
@@ -3,7 +3,6 @@ package jetbrains.buildServer.clouds.kubernetes.web;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import jetbrains.buildServer.clouds.kubernetes.RequestKubeApiConnection;
@@ -14,7 +13,10 @@ import jetbrains.buildServer.clouds.kubernetes.connector.KubeApiConnector;
 import jetbrains.buildServer.clouds.kubernetes.connector.KubeApiConnectorImpl;
 import jetbrains.buildServer.controllers.BaseController;
 import jetbrains.buildServer.serverSide.IOGuard;
-import jetbrains.buildServer.serverSide.identifiers.ProjectIdentifiersManager;
+import jetbrains.buildServer.serverSide.ProjectManager;
+import jetbrains.buildServer.serverSide.SProject;
+import jetbrains.buildServer.serverSide.SecurityContextEx;
+import jetbrains.buildServer.serverSide.auth.SecurityContext;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.WebControllerManager;
 import org.jetbrains.annotations.NotNull;
@@ -26,6 +28,8 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public abstract class ChooserController extends BaseController {
 
+    private final ProjectManager myProjectManager;
+    private final SecurityContext mySecurityContext;
     private final PluginDescriptor myPluginDescriptor;
     private final KubeAuthStrategyProvider myAuthStrategyProvider;
     private final KubernetesCredentialsFactory myCredentialsFactory;
@@ -34,16 +38,23 @@ public abstract class ChooserController extends BaseController {
                              @NotNull PluginDescriptor pluginDescriptor,
                              @NotNull KubeAuthStrategyProvider authStrategyProvider,
                              @NotNull KubernetesCredentialsFactory credentialsFactory,
-                             @NotNull String pluginResourcesPath) {
-        myPluginDescriptor = pluginDescriptor;
-        myAuthStrategyProvider = authStrategyProvider;
-        myCredentialsFactory = credentialsFactory;
-        web.registerController(pluginDescriptor.getPluginResourcesPath(pluginResourcesPath), this);
+                             @NotNull String pluginResourcesPath,
+                             @NotNull SecurityContext securityContext,
+                             @NotNull ProjectManager projectManager) {
+      myProjectManager = projectManager;
+      myPluginDescriptor = pluginDescriptor;
+      myAuthStrategyProvider = authStrategyProvider;
+      myCredentialsFactory = credentialsFactory;
+      mySecurityContext = securityContext;
+      web.registerController(pluginDescriptor.getPluginResourcesPath(pluginResourcesPath), this);
     }
 
     @Nullable
     @Override
-    protected ModelAndView doHandle(@NotNull HttpServletRequest httpServletRequest, @NotNull HttpServletResponse httpServletResponse) throws Exception {
+    protected ModelAndView doHandle(@NotNull HttpServletRequest httpServletRequest, @NotNull HttpServletResponse httpServletResponse) {
+
+        checkPermissions(httpServletRequest);
+
         KubeApiConnection apiConnection = new RequestKubeApiConnection(httpServletRequest);
         ModelAndView modelAndView = new ModelAndView(myPluginDescriptor.getPluginResourcesPath(getJspName()));
         try (KubeApiConnector apiConnector
@@ -62,6 +73,15 @@ public abstract class ChooserController extends BaseController {
         return modelAndView;
     }
 
+  private void checkPermissions(HttpServletRequest request) {
+    String projectId = request.getParameter("projectId");
+    SProject project = myProjectManager.findProjectByExternalId(projectId);
+    if (project == null) {
+      throw new IllegalArgumentException("Project was not found by projectId: " + projectId);
+    }
+    ((SecurityContextEx)mySecurityContext).getAccessChecker().checkCanEditProject(project);
+  }
+
     @NotNull
     protected abstract Collection<String> getItems(KubeApiConnector apiConnector);
 
@@ -75,19 +95,23 @@ public abstract class ChooserController extends BaseController {
       public static final String KUBE_DEPLOYMENTS_HTML = "kubeDeployments.html";
       private static String controllerUrl;
 
-      public Deployments(final WebControllerManager web,
-                         final PluginDescriptor pluginDescriptor,
-                         final KubeAuthStrategyProvider authStrategyProvider,
-                         final KubernetesCredentialsFactory kubernetesCredentialsFactory) {
-            super(web, pluginDescriptor, authStrategyProvider, kubernetesCredentialsFactory, KUBE_DEPLOYMENTS_HTML);
+      public Deployments(@NotNull final WebControllerManager web,
+                         @NotNull final PluginDescriptor pluginDescriptor,
+                         @NotNull final KubeAuthStrategyProvider authStrategyProvider,
+                         @NotNull final KubernetesCredentialsFactory kubernetesCredentialsFactory,
+                         @NotNull SecurityContext securityContext,
+                         @NotNull ProjectManager projectManager) {
+            super(web, pluginDescriptor, authStrategyProvider, kubernetesCredentialsFactory, KUBE_DEPLOYMENTS_HTML, securityContext, projectManager);
             controllerUrl = pluginDescriptor.getPluginResourcesPath(KUBE_DEPLOYMENTS_HTML);
         }
 
+        @NotNull
         @Override
         protected Collection<String> getItems(final KubeApiConnector apiConnector) {
             return apiConnector.listDeployments();
         }
 
+        @NotNull
         @Override
         protected String getItemsName() {
             return "deployments";
@@ -107,19 +131,23 @@ public abstract class ChooserController extends BaseController {
       public static final String KUBE_NAMESPACES_HTML = "kubeNamespaces.html";
       private static String controllerUrl;
 
-      public Namespaces(final WebControllerManager web,
-                        final PluginDescriptor pluginDescriptor,
-                        final KubeAuthStrategyProvider authStrategyProvider,
-                        final KubernetesCredentialsFactory kubernetesCredentialsFactory) {
-            super(web, pluginDescriptor, authStrategyProvider, kubernetesCredentialsFactory, KUBE_NAMESPACES_HTML);
-            controllerUrl = pluginDescriptor.getPluginResourcesPath(KUBE_NAMESPACES_HTML);
+      public Namespaces(@NotNull final WebControllerManager web,
+                        @NotNull final PluginDescriptor pluginDescriptor,
+                        @NotNull final KubeAuthStrategyProvider authStrategyProvider,
+                        @NotNull final KubernetesCredentialsFactory kubernetesCredentialsFactory,
+                        @NotNull SecurityContext securityContext,
+                        @NotNull ProjectManager projectManager) {
+          super(web, pluginDescriptor, authStrategyProvider, kubernetesCredentialsFactory, KUBE_NAMESPACES_HTML, securityContext, projectManager);
+          controllerUrl = pluginDescriptor.getPluginResourcesPath(KUBE_NAMESPACES_HTML);
         }
 
+        @NotNull
         @Override
         protected Collection<String> getItems(final KubeApiConnector apiConnector) {
             return apiConnector.listNamespaces();
         }
 
+        @NotNull
         @Override
         protected String getItemsName() {
             return "namespaces";
